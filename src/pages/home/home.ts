48d8750f4d1b515284d71b10ad4ec59855b9ae57
Khalid <khalid@bawes.net>
2017-04-30 22:46:19 +0300

    File manipulation via form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AwsService } from '../../providers/aws.service'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  public AWS_IAM_USER = "app_public";
-  public BUCKET_NAME = "123";
+  public file: File;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private _aws: AwsService
+    ) {
 
+  }
+
+  /**
+   * Login to upload the file
+   */
+  uploadFile($event){
+    let fileList: FileList = $event.target.files;
+
+    // Check if files available
+    if(fileList.length > 0){
+      let file: File = fileList.item(0);
+      console.log(file.name);
+    }
+    
   }
 
 }
